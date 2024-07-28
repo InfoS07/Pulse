e5185740d1cb5943cb24067b17f9d34ef05f4356
L-Nicolas <nicolas.lacoste.esgi@gmail.com>
2024-07-28 23:48:25 +0200

    Correction api url, remplacer par heroku

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,6 +1,6 @@
 class AppSecrets {
   static const apiGraphqlUrl =
-      'http://test-lb-tf-405076187.eu-west-1.elb.amazonaws.com/graphql';
+      'https://pulse-6d0ccf9c65eb.herokuapp.com/graphql';
   static const supabaseUrl = 'https://xbxtezmargogbsninnby.supabase.co';
   static const supabaseAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhieHRlem1hcmdvZ2JzbmlubmJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM3MTEwMjgsImV4cCI6MjAyOTI4NzAyOH0.8P1GKYPpRT-gDtpX_rseS9c80TiZJ1c4M8vLTQbgp0A';
